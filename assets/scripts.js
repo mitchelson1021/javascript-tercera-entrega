c7aef6df796c9317690157456833eea12df094fa
mitchelson1021 <mixander@gmail.com>
2023-03-02 02:54:42 -0300

    corrigiendo detalles en el filter, trabajando las cards

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,16 +1,5 @@
 
 
-let productos = [
-    {
-        "id":1,
-        "nombre":"",
-        "imagen":"",
-        "descripcion":"",
-        "precio":"",
-        "oferta":true,
-        "signo":""
-}]
-
 
 
 function zodiaco () {
@@ -18,19 +7,6 @@ function zodiaco () {
     let mes = document.getElementById("mes");
     let año = document.getElementById("anio");
     let mensaje = document.getElementById("mensaje");
-
-    function fechaNacimiento (signo) {
-        let textMensaje = document.createElement("div");
-        textMensaje.innerHTML = `<h3>Si tu fecha de Nacimiento es ${dia.value} / ${mes.value} / ${año.value}  tu signo del Zodiaco es "${signo}"`  
-        mensaje.append(textMensaje);
-    }
-
-    function error () {
-        let textMensajeError = document.createElement("h2");
-        textMensajeError.innerHTML = "Hubo un error al ingresar los datos, intenta de nuevo";
-        mensaje.append(textMensajeError);
-
-    }
 
         //Aries
     if (dia.value >= 21 && dia.value <= 31 &&  mes.value == "3" || dia.value >= 1 && dia.value <= 19 &&  mes.value == "4") {
@@ -86,16 +62,66 @@ function zodiaco () {
         console.log("Revisa los parámetros ingresados, hubo un error")
     }
 
+    function fechaNacimiento (signo) {
+        let textMensaje = document.createElement("div");
+        textMensaje.innerHTML = `<h3>Si tu fecha de Nacimiento es ${dia.value} / ${mes.value} / ${año.value}  tu signo del Zodiaco es "${signo}"`  
+        mensaje.append(textMensaje);
+    }
+
+    function error () {
+        let textMensajeError = document.createElement('h3');
+        textMensajeError.innerHTML = `<h3>Hubo un error al ingresar los datos, intenta de nuevo</h3>`;
+        mensaje.append(textMensajeError);
+
+    }
+
 }
 
+//-------------------Filtro------------------
 
 function btn_select () {
     let btn_select_signo = document.getElementById("select").value
         console.log("respuesta al click del btn " + btn_select_signo)
-        console.log(signos)
-        let filter = signos.filter(function (filtrado) {
-            return filtrado.signo === btn_select_signo
-        });
-        console.log(filter)
+        
+        let filtro = signos.filter(buscar => buscar.signo == btn_select_signo);
+        console.log(filtro);
+//-------Aquí me funciona en consola pero no en el Dom--------
+        let datos_signo = document.createElement('div');
+        datos_signo.innerHTML = `<p>${filtro}</p>`;
+        mensaje.append(datos_signo);
+
+}
+
+//-------------------Zona de Ventas-------------
+
+
+
+let carrito = [];
+
+
+
+function agregar_al_carrito (e){
+    console.log("se agregó producto al carrito");
+
+    let hijo = e.target;
+    let padre = hijo.parentNode;
+    let abuelo = padre.parentNode;
+
+
+    let nombre_producto = padre.querySelector("h5").textContent;
+    let precio_producto = padre.querySelector("p").textContent;
+    let imagen_ptoducto = abuelo.querySelector("img").src;
+
+    console.log(nombre_producto);
+    console.log(precio_producto);
+    console.log(imagen_ptoducto);
+
+}
+
+let btn_comprar = document.querySelectorAll(".btn_agregar");
+
+for (comprar of btn_comprar){
+    comprar.addEventListener("click", agregar_al_carrito);
+    
 
 }
